72b4dfb0ec236c94e73adceb401de0224cf16155
jaume vidal <jaumevibu@gmail.com>
2023-04-14 12:25:09 +0200

    first commit

---
+++ src/main/java/com/accenture/techEventsBack/TechEventsBackApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.techEventsBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEventsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEventsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/techEventsBack/TechEventsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.techEventsBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechEventsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
